632c09a02bc449e2fbda2e0090063f1c54a3bf00
MilaMima <milani.kalitko.3012@gmail.com>
2023-08-31 10:39:03 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import { createGlobalStyle } from 'styled-components'
+import { Roboto } from 'next/font/google'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// If loading a variable font, you don't need to specify the font weight
+const roboto = Roboto({ weight: '400', subsets: ['latin'] })
+
+const GlobalStyles = createGlobalStyle`
+  body{
+    padding: 0;
+    margin: 0;
+    font-family: ${roboto.style.fontFamily};
+  }
+`
+
+export default function App ({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }

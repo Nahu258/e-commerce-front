d3a4a1a050c2046575c6f33180b5ad3c368db235
Nahuel <Nahugc258@gmail.com>
2023-09-27 20:40:15 -0300

    Add product/id page its components

---
+++ pages/product/[id].js
@@ -0,0 +1,77 @@
+import Center from '@/components/Center'
+import Header from '@/components/Header'
+import { mongooseConnect } from '@/lib/mongoose'
+import { Product } from '@/models/Product'
+import styled from 'styled-components'
+import ProductImages from '@/components/ProductImages'
+import Button from '@/components/Button'
+import CartIcon from '@/components/icons/CartIcon'
+import { useContext } from 'react'
+import { CartContext } from '@/context/CartContext'
+
+const ColWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: .8fr 1.2fr;
+  }
+  gap: 40px;
+  margin: 40px 0;
+`
+const PriceRow = styled.div`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+`
+const Price = styled.span`
+  font-size: 1.4rem;
+`
+const Title = styled.h1`
+  font-size: 1.5em;
+`
+const WhiteBox = styled.div`
+  background-color: #fff;
+  border-radius: 10px;
+  padding: 30px;
+`
+
+export default function ProductPage ({ product }) {
+  const { addProduct } = useContext(CartContext)
+  return (
+    <>
+      <Header />
+      <Center>
+        <ColWrapper>
+          <WhiteBox>
+            <ProductImages images={product.images} />
+          </WhiteBox>
+          <div>
+            <Title>{product.title}</Title>
+            <p>{product.description}</p>
+            <PriceRow>
+              <div>
+                <Price>${product.price}</Price>
+              </div>
+              <div>
+                <Button $primary onClick={() => addProduct(product._id)}>
+                  <CartIcon />Add to cart
+                </Button>
+              </div>
+            </PriceRow>
+          </div>
+        </ColWrapper>
+      </Center>
+    </>
+  )
+}
+
+export async function getServerSideProps (context) {
+  await mongooseConnect()
+  const { id } = context.query
+  const product = await Product.findById(id)
+  return {
+    props: {
+      product: JSON.parse(JSON.stringify(product))
+    }
+  }
+}
